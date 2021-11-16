a6bdf883850ebe483e7b541c87ce76cf65c088f9
Alexandr <alshumkov2@gmail.com>
2021-11-16 19:00:25 +0300

    Найти расстояние между точками в пространстве 2D/3D

---
+++ Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿//22. Найти расстояние между точками в пространстве 2D/3D
+
+double Distance2D(double x1,double y1,double x2,double y2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+}
+
+Console.WriteLine(Distance2D(1,1,3,4));
