1d307d053e31952505c15d82d1f58b027d8e0878
Alexandr <alshumkov2@gmail.com>
2021-11-16 17:36:32 +0300

    Определить кол-во цифр в числе

---
+++ Task28/Program.cs
@@ -0,0 +1,12 @@
+﻿//27.Определить кол-во цифр в числе
+int CountDigit(int n)
+{
+    int k=0;
+    while(n!=0)
+    {
+        k++;
+        n=n/10;
+    }
+    return k;
+}
+Console.WriteLine(CountDigit(576483));
