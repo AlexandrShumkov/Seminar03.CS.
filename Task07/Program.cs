9237e1dca2a4ba25c260e54ce233edd19a7f605e
Alexandr <alshumkov2@gmail.com>
2021-11-17 17:36:15 +0300

    6. Выяснить является ли число чётным

---
+++ Task07/Program.cs
@@ -0,0 +1,13 @@
+﻿// 6. Выяснить является ли число чётным
+
+int a;
+Console.WriteLine("Напишите ваше число:");
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
