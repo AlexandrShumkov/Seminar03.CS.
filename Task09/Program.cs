81ee3729fefe67289ef4d8fb59aab88505a50cfe
Alexandr <alshumkov2@gmail.com>
2021-11-17 19:45:28 +0300

    8. Показать четные числа от 1 до N

---
+++ Task09/Program.cs
@@ -0,0 +1,18 @@
+﻿//8. Показать четные числа от 1 до N
+
+Console.WriteLine("Введите число N");
+int N;
+N=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Четные числа от 1 до N");
+
+int index;
+index=1;
+while(index<N)
+{
+    if(index%2==0)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
