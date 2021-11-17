855ccaf50be4041002901911be73219e05cd34de
Alexandr <alshumkov2@gmail.com>
2021-11-17 16:36:35 +0300

    34. Написать программу замену элементов массива на противоположные

---
+++ Task35/Program.cs
@@ -0,0 +1,14 @@
+﻿//34. Написать программу замену элементов массива на противоположные
+
+int[] arr=new int[10];
+Random rnd=new Random();
+int i;
+for(i=0;i<arr.Length;i++)
+{
+arr[i]=rnd.Next(1,100);
+}
+for(i=0;i<arr.Length;i++)
+Console.Write(arr[i]+" ");
+Console.WriteLine();
+for(i=0;i<arr.Length;i++)
+Console.Write(-arr[i]+" ");
