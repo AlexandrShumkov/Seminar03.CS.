ee3e6f2fa8f3a1af84e3233bbf70f4b2c5b9032e
Alexandr <alshumkov2@gmail.com>
2021-11-17 14:06:27 +0300

    35. Определить, присутствует ли в заданном массиве, некоторое число

---
+++ Task36/Program.cs
@@ -0,0 +1,27 @@
+﻿//35. Определить, присутствует ли в заданном массиве, некоторое число
+
+int[] arr = new int[100];
+Console.WriteLine("Введите число от 1 до 100: ");
+Random rnd = new Random();
+int i;
+//Console.ReadLine("Введите число от 1 до 100: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(1, 101);
+    if (arr[i] == a)
+    {
+        Console.WriteLine("Число присутствует" + " " + a);
+        break;
+    }
+}
+if(i==arr.Length)
+Console.WriteLine("Число не найдено");
+
+
+
+
+
+
+
