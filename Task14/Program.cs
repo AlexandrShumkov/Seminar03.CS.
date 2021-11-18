370030c33ce9a2f44faf61ea5f4e05eb9bcaa203
Alexandr <alshumkov2@gmail.com>
2021-11-18 15:53:02 +0300

    13. Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ Task14/Program.cs
@@ -0,0 +1,11 @@
+﻿//13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+double a=14;double b=7;
+if(a%b==0)
+{
+    Console.WriteLine("Без остатка");
+}
+else
+{
+    Console.WriteLine(a%b);
+}
