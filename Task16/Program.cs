2f9805dfa8acf54ce761c20660d30bcda9ae6622
Alexandr <alshumkov2@gmail.com>
2021-11-18 17:26:38 +0300

    15. Дано число. Проверить кратно ли оно 7 и 23

---
+++ Task16/Program.cs
@@ -0,0 +1,12 @@
+﻿//15. Дано число. Проверить кратно ли оно 7 и 23
+
+Console.WriteLine("Введите число от 1 до 1000: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if((a%7==0)&&(a%23==0))
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Некратно");
+}
