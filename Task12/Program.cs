609fa6a32e02a810536b2a9d080385587fe973e3
Alexandr <alshumkov2@gmail.com>
2021-11-18 13:13:18 +0300

    11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ Task12/Program.cs
@@ -0,0 +1,14 @@
+﻿//11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+
+Console.WriteLine("Напишите число от 10 до 99: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b=a%10; int c=a/10;
+if ( b>c)
+{
+    Console.WriteLine("Наибольшая цифра: "+b);
+}
+else
+    Console.WriteLine("Наибольшая цифра: "+c);
+
+
