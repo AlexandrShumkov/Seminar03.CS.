b059a6b56ba7c9f4cb6f4619590e1b5e82c0634f
Alexandr <alshumkov2@gmail.com>
2021-11-18 12:18:53 +0300

    задача егэ

---
+++ TaskEGE_01/Program.cs
@@ -0,0 +1,13 @@
+﻿//Условие задачи в конспекте.Нашли только числа имеющие 4 делителя,кроме 1 и самого числа
+int CountDiv(int n)
+{
+    int k = 0;
+    for (int i = 2; i < n; i++)
+        if (n % i == 0) k++;
+    return k;
+}
+for(int i=210235;i<=210300;i++)
+if(CountDiv(i)==4)
+{
+    Console.WriteLine(i);
+}
