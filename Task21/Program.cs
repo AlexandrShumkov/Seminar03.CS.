ec61bfd45e741f99e1fcc9d52967fd80f25fbb6f
Alexandr <alshumkov2@gmail.com>
2021-11-23 10:59:52 +0300

    20. Ввести номер четверти, показать диапазоны для возможных координат

---
+++ Task21/Program.cs
@@ -0,0 +1,10 @@
+﻿// 20. Ввести номер четверти, показать диапазоны для возможных координат
+
+void FindQuater(int x, int y)
+{
+    if ((x > 0) && (y > 0)) Console.WriteLine("1я четверть");
+    if ((x < 0) && (y > 0)) Console.WriteLine("2я четверть");
+    if ((x < 0) && (y < 0)) Console.WriteLine("3я четверть");
+    if ((x > 0) && (y < 0)) Console.WriteLine("4я четверть");
+}
+FindQuater(4,-4);
