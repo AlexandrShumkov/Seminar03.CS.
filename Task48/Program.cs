d1b28626f85d0f354357ea1ec0bd9c82d23ccba4
Alexandr <alshumkov2@gmail.com>
2021-11-29 18:26:30 +0300

    47. Написать программу копирования массива

---
+++ Task48/Program.cs
@@ -0,0 +1,21 @@
+﻿// 47. Написать программу копирования массива
+
+int[] arr = new int[10];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(1, 10);
+    Console.Write(arr[i] + " ");
+}
+Console.WriteLine();
+
+int[] arr1=new int[10];
+for (int i = 0; i < arr.Length; i++)
+
+    {
+        arr1[i] = arr[i];
+        Console.Write(arr1[i]+" ");
+    }
+
+
+
