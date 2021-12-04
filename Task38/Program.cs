8b47a59df960aa07348a7fbe099ed9a488011704
Alexandr <alshumkov2@gmail.com>
2021-12-04 14:57:44 +0300

    37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]

---
+++ Task38/Program.cs
@@ -0,0 +1,37 @@
+﻿//37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] arr = new int[123];
+Random rnd = new Random();
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(1, 124);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for(int i=0;i<arr.Length;i++)
+    {
+        Console.Write(arr[i]+" ");
+    }
+    Console.WriteLine();
+}
+
+void CountDigit(int[] arr,int count)
+{
+     count=0;
+    for(int i=10;i<=99;i++)
+    {
+        count++;
+    }
+        Console.WriteLine($"{count} чисел в отрезке.");
+}
+
+int count=0;
+FillArray(arr);
+PrintArray(arr);
+Console.ReadKey();
+CountDigit(arr,count);
